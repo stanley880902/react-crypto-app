ff886bb7cbf870ecf1a87f48e6ed7fc784e4f570
Stanley Tsai <stanley880902@gmail.com>
2023-03-15 17:11:33 -0700

    the graph in show page  would not show

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,6 +1,52 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import showStore from '../stores/showStore';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
 
 export default function Show() {
   const store = showStore();
@@ -10,7 +56,33 @@ export default function Show() {
     store.fetchData(params.id)
   }, [])
   
+  if (!store.data) return <></>
+
   return (
-    <div>Show</div>
+    <div>
+      <header>
+        <img src={store.data.img.large} />
+        <h2>
+          {store.data.name} ({store.data.symbol})
+        </h2>
+      </header>
+      <AreaChart
+      width={500}
+      height={400}
+      data={store.graphData}
+      margin={{
+        top: 10,
+        right: 30,
+        left: 0,
+        bottom: 0,
+      }}
+      >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="Date" />
+      <YAxis />
+      <Tooltip />
+      <Area type="monotone" dataKey="Price" stroke="#8884d8" fill="#8884d8" />
+      </AreaChart>
+    </div>
   )
 }
--- src/stores/showStore.js
+++ src/stores/showStore.js
@@ -3,9 +3,28 @@ import axios from 'axios';
 import debounce from '../helper/debounce';
 
 const showStore = create((set) => ({
-    fetchData: (id) => {
-        console.log('hey', id);
+    graphData: [],
+
+    fetchData: async (id) => {
+        const [graphRes, dataRes] = await Promise.all([
+            axios.get(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=121`),
+            axios.get(`https://api.coingecko.com/api/v3/coins/${id}?localization=false&market_data=true`),
+        ]);
+        
+        const graphData = graphRes.data.prices.map((price) => {
+            const [timeStamp, p] = price;
+            const date = new Date(timeStamp).toLocaleDateString('en-us');
+
+            return {
+                Date: date,
+                Price: p
+              };
+        });
+        console.log(dataRes);
+        set({ graphData });
     },
 }));
+
+
 
 export default showStore;
